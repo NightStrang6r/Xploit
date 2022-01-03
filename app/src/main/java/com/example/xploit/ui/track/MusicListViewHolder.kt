f0bf3a561265e0d9cd033ca11b361f1a41430676
NightStrang6r <tsaruk-2015@mail.ru>
2022-01-03 16:09:49 +0200

    🐞 Fixed images displaying

--- app/src/main/java/com/example/xploit/ui/track/MusicListViewHolder.kt
+++ app/src/main/java/com/example/xploit/ui/track/MusicListViewHolder.kt
@@ -7,9 +7,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.xploit.R
 import com.squareup.picasso.Picasso
 import android.content.Intent
+import android.util.Log
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.xploit.ui.musicplayer.MusicPlayerActivity
 import androidx.core.content.ContextCompat.startActivity
+import com.example.xploit.api.BASE_URL
+import java.net.URLEncoder
 
 const val KEY_DATA_music_list = "data_music_list"
 
@@ -22,12 +25,14 @@ class MusicListViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
         val cover = itemView.findViewById<ImageView>(R.id.iv_logo)
 
         val durationVal = item.playTime.substring(3)
+        val imgCover = URLEncoder.encode("${item.imgCover}", "utf-8")
+        val readyImageCover = "${BASE_URL}img/?url=${imgCover}"
 
         name.text = item.name
         artist.text = item.artist
         duration.text = durationVal
         Picasso.with(itemView.context)
-            .load(item.imgCover)
+            .load(readyImageCover)
             .placeholder(R.drawable.track_cover)
             .into(cover)
 
